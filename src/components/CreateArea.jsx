2ec8cc7bf98e07786fec33ba3ac056c33ad9a591
Laksamana <103801675+laksamanaap@users.noreply.github.com>
2023-09-03 22:31:48 +0700

    feat : convert to spread operator

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,42 +1,59 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
-  const [inputHeader, setInputHeader] = useState();
-  const [inputText, setInputText] = useState();
+  const [note, setNote] = useState({
+    titlea: "",
+    contentb: ""
+  });
 
-  function handleTextChange(e) {
-    const { value } = e.target;
-    setInputText(value);
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setNote((prevNote) => {
+      console.log("Inside Note : ", prevNote);
+      return { ...prevNote, [name]: value };
+    });
   }
 
-  function handleHeaderChange(e) {
-    const { value } = e.target;
-    setInputHeader(value);
-  }
+  // const [inputHeader, setInputHeader] = useState();
+  // const [inputText, setInputText] = useState();
+
+  // function handleTextChange(e) {
+  //   const { value } = e.target;
+  //   setInputText(value);
+  // }
+
+  // function handleHeaderChange(e) {
+  //   const { value } = e.target;
+  //   setInputHeader(value);
+  // }
 
   return (
     <div>
       <form>
         <input
-          name="title"
+          name="titlea"
           placeholder="Title"
-          onChange={handleHeaderChange}
-          value={inputHeader}
+          onChange={handleChange}
+          value={note.titlea}
         />
         <textarea
-          name="content"
+          name="contentb"
           placeholder="Take a note..."
           rows="3"
-          onChange={handleTextChange}
-          value={inputText}
+          onChange={handleChange}
+          value={note.contentb}
         />
         <button
           type="submit"
           onClick={(e) => {
             e.preventDefault();
-            props.onAdd(inputText, inputHeader);
-            // setInputHeader("");
-            // setInputText("");
+            props.onAdd(note.contentb, note.titlea);
+            setNote({
+              titlea: "",
+              contentb: ""
+            });
+            // console.log(note.title);
+            // console.log(note.content);
           }}
         >
           Add
